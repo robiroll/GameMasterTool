169a360bd4caf907035b4a6d7739afac700f05bd
Robinson <robinson@pretto.fr>
2018-01-10 23:13:47 +0100

    added end fight action

--- src/Components/Dashboard/index.js
+++ src/Components/Dashboard/index.js
@@ -4,11 +4,12 @@ import Characters from '../../Containers/Characters'
 import Button from '../../styleguide/Button'
 // import './Dashboard.css'
 
-const Dashboard = ({ currentTurn, onEndTurn }) => {
+const Dashboard = ({ currentTurn, onEndTurn, onEndFight }) => {
   return (
     <div className="dashboard">
       <div className="turn">Tour {currentTurn}</div>
       <Button onClick={onEndTurn}>end turn</Button>
+      <Button onClick={onEndFight}>end fight</Button>
       <Characters />
     </div>
   )
@@ -16,7 +17,8 @@ const Dashboard = ({ currentTurn, onEndTurn }) => {
 
 Dashboard.propTypes = {
   currentTurn: PropTypes.number,
-  onEndTurn: PropTypes.func
+  onEndTurn: PropTypes.func,
+  onEndFight: PropTypes.func
 }
 
 export default Dashboard
--- src/Containers/Dashboard/index.js
+++ src/Containers/Dashboard/index.js
@@ -5,10 +5,9 @@ export default class Dashboard extends Component {
   state = {
     turn: 0
   }
-  onEndTurn = () => {
-    this.setState({ turn: this.state.turn + 1 })
-  }
+  onEndTurn = () => this.setState({ turn: this.state.turn + 1 })
+  onEndFight = () => this.setState({ turn: 0 })
   render() {
-    return <DashboardComponent currentTurn={this.state.turn} onEndTurn={this.onEndTurn} />
+    return <DashboardComponent currentTurn={this.state.turn} onEndTurn={this.onEndTurn} onEndFight={this.onEndFight} />
   }
 }
